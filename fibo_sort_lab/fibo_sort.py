fc8d0f1b2eeb16bd1ca6a98d23cd8bf48ccd91ae
Ivan Krasnoshchok <46425174+i1red@users.noreply.github.com>
2019-09-15 18:50:42 +0300

    Add files via upload

--- fibo_sort_lab/fibo_sort.py
+++ fibo_sort_lab/fibo_sort.py
@@ -1,1 +1,30 @@
+import fibo_sort_lab.filetools as ftools
+import uuid
+import os
 
+
+def divide(chunks_quantity, files_quantity):
+    distribution = [0 for i in range(files_quantity)]
+    tmp = list(distribution)
+    distribution[0] = 1
+
+    while sum(distribution) < chunks_quantity:
+        max_val = max(distribution)
+        index = distribution.index(max_val)
+        for i in range(files_quantity):
+            tmp[i] = 0 if i == index else tmp[i] + max_val
+        distribution = list(tmp)
+
+    return distribution
+
+
+def fibo_sort(filename: str, chunk_size: 'int > 0', files_quantity: 'int > 2'):
+    temp_files_dir = str(uuid.uuid4().hex)
+    chunks_quantity = os.path.getsize(filename) // 4
+    distribution = divide(chunks_quantity, files_quantity)
+    with ftools.create_temp_files(temp_files_dir, files_quantity) as files:
+        for f in files:
+            print(f)
+
+
+fibo_sort('test.bin', 23, 3)
---
+++ fibo_sort_lab/filetools.py
@@ -0,0 +1,45 @@
+import os
+import shutil
+import random
+import uuid
+from contextlib import contextmanager
+
+
+def write(file, num):
+    num_bytes = int.to_bytes(num, 4, byteorder='little', signed=True)
+    file.write(num_bytes)
+
+
+def read(file):
+    num_bytes = file.read(4)
+    return int.from_bytes(num_bytes, byteorder='little', signed=True) if len(num_bytes) == 4 else None
+
+
+def clear(*files):
+    for file in files:
+        file.seek(0)
+        file.truncate()
+
+
+@contextmanager
+def create_temp_files(directory: str, files_quantity: 'int > 0'):
+    try:
+        os.mkdir(directory)
+        files = [open(f'{directory}/{i}.bin', 'wb') for i in range(files_quantity)]
+        filenames = [f.name for f in files]
+        for f in files:
+            f.close()
+        files = [open(fname, 'rb+') for fname in filenames]
+        yield files
+    finally:
+        for f in files:
+            f.close()
+        shutil.rmtree(directory, ignore_errors=True)
+
+
+def generate_test_file(file_title: str, num_quantity: 'int > 0'):
+    with open(file_title + '.bin', 'wb') as f:
+        num_count = 0
+        while num_count < num_quantity:
+            write(f, random.randint(-2147483648, 2147483647))
+            num_count += 1
