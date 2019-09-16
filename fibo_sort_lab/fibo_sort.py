0cbfc6b97d660ee013a160d12b7c27387732aa3b
i1red <ivankras67@gmail.com>
2019-09-17 01:04:00 +0300

    Works for ideal distribution

--- fibo_sort_lab/fibo_sort.py
+++ fibo_sort_lab/fibo_sort.py
@@ -1,9 +1,11 @@
-import fibo_sort_lab.filetools as ftools
-import uuid
 import os
+import math
+import uuid
+import fibo_sort_lab.filetools as ftools
+import fibo_sort_lab.priorityqueue as pq
 
 
-def divide(chunks_quantity, files_quantity):
+def hor_distr(chunks_quantity, files_quantity):
     distribution = [0 for i in range(files_quantity)]
     tmp = list(distribution)
     distribution[0] = 1
@@ -15,16 +17,101 @@ def divide(chunks_quantity, files_quantity):
             tmp[i] = 0 if i == index else tmp[i] + max_val
         distribution = list(tmp)
 
-    return distribution
+    return sorted(distribution, reverse=True)
 
 
-def fibo_sort(filename: str, chunk_size: 'int > 0', files_quantity: 'int > 2'):
+def fibo_sort(source_file: str, run_size: 'int > 0', files_quantity: 'int > 2'):
     temp_files_dir = str(uuid.uuid4().hex)
-    chunks_quantity = os.path.getsize(filename) // 4
-    distribution = divide(chunks_quantity, files_quantity)
+    runs_quantity = math.ceil(os.path.getsize(source_file) / (run_size * 4))
+
+    distr = hor_distr(runs_quantity, files_quantity)
+    dummy = []
+
     with ftools.create_temp_files(temp_files_dir, files_quantity) as files:
-        for f in files:
-            print(f)
+        with open(source_file, 'rb') as source:
+            for i in range(len(distr)):
+                el_count = 0
+                while el_count < distr[i]:
+                    chunk, j = [], 0
+                    while j < run_size:
+                        num = ftools.read(source)
+                        if num is not None:
+                            chunk.append(num)
+                        j += 1
+                    chunk.sort()
+                    for el in chunk:
+                        ftools.write(files[i], el)
+                    el_count += 1
+
+        qu = pq.PriorityQueueBST()
+
+        runs_len = [run_size for i in range(files_quantity)]
+        el_read = [0 for i in range(files_quantity)]
+        file_ptrs = [0 for i in range(files_quantity)]
+
+        output = 0
+        while max(distr) != 1 or distr.count(0) != len(distr) - 1:
+
+            for i, f in enumerate(files):
+                f.seek(file_ptrs[i])
+
+            output = res_pos = distr.index(0)
+
+            while min(dst for i, dst in enumerate(distr) if i != output) != 0:
+                for i in range(files_quantity):
+                    if i != output:
+                        if el_read[i] < runs_len[i]:
+                            num = ftools.read(files[i])
+                            el_read[i] += 1
+                            if num is not None:
+                                qu.append(num, i)
+
+                while len(qu) > 0:
+                    min_el_val, min_el_index = qu.pop()
+                    print(min_el_val, end=' ')
+                    ftools.write(files[output], min_el_val)
+                    if el_read[min_el_index] < runs_len[min_el_index]:
+                        num = ftools.read(files[min_el_index])
+                        el_read[min_el_index] += 1
+                        if num is not None:
+                            qu.append(num, min_el_index)
+
+                for i in range(files_quantity):
+                    el_read[i] = 0
+                    distr[i] = distr[i] - 1 if i != output else distr[i] + 1
+                    if i != output:
+                        file_ptrs[i] = files[i].tell()
+
+                runs_len[output] = sum(r_len for i, r_len in enumerate(runs_len) if i != output)
+
+        print(files[output].tell())
+        files[output].seek(file_ptrs[output])
+        print(files[output].tell())
+        with open('res.bin', 'wb') as res:
+            num = ftools.read(files[output])
+            while num is not None:
+                ftools.write(res, num)
+                num = ftools.read(files[output])
 
 
-fibo_sort('test.bin', 23, 3)
+
+
+
+fibo_sort('test.bin', 8, 6)
+
+a, b = [], []
+with open('res.bin', 'rb') as f:
+    num = ftools.read(f)
+    while num is not None:
+        a.append(num)
+        num = ftools.read(f)
+
+with open('test.bin', 'rb') as f:
+    num = ftools.read(f)
+    while num is not None:
+        b.append(num)
+        num = ftools.read(f)
+
+print(a)
+print(b)
+print(a == sorted(b))
